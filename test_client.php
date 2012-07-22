c16fb71f316464d4039f949a3b63840205ed9e95
ddmbr <iddmbr@gmail.com>
2012-07-22 11:01:36 +0400

    auto connect

--- test_client.php
+++ test_client.php
@@ -1,26 +1,48 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
-<head>
-  <title>OpenTok Getting Started</title>
-  <script src="http://staging.tokbox.com/v0.91/js/TB.min.js"></script>
- 
-  <script type="text/javascript">
-    var apiKey = '1127';
-    var sessionId = '14685d1ac5907f4a2814fed28294d3f797f34955';
-    var token = 'devtoken';           
- 
-    TB.setLogLevel(TB.DEBUG); // Set this for helpful debugging messages in console
- 
-    var session = TB.initSession(sessionId);      
-    session.addEventListener('sessionConnected', sessionConnectedHandler);      
-    session.connect(apiKey, token);
- 
-    function sessionConnectedHandler(event) {
-      alert('Hello world. I am connected to OpenTok :).');
-    }     
-  </script>
- 
-</head>
- 
-<body>
-</body>
+    <head>
+        <?php
+            require_once 'API_Config.php';
+            require_once 'test_server.php';
+        ?>
+        <title>OpenTok Getting Started</title>
+        <script src="http://staging.tokbox.com/v0.91/js/TB.min.js"></script>
+
+        <script type="text/javascript">
+            var apiKey = '<?php echo API_Config::API_KEY?>';
+            var sessionId = '<?php echo $sessionID ?>';
+            var token = '<?php echo $token ?>';           
+            var basicStreams = [];
+
+            TB.setLogLevel(TB.DEBUG); // Set this for helpful debugging messages in console
+
+            var session = TB.initSession(sessionId);      
+            session.addEventListener('sessionConnected', sessionConnectedHandler);      
+            session.connect(apiKey, token);
+
+            function sessionConnectedHandler(event) {
+                alert('Hello world. I am connected to OpenTok :).');
+                session.publish();
+                for (var i = 0; i < event.streams.length; i++) {
+                    if (event.streams[i].type == "basic") {
+                        subscribeToStream(event.target, event.streams[i]);
+                        basicStreams[event.streams[i].streamId] = event.streams[i];
+                    }
+                }
+            }
+
+            function subscribeToStream(session, stream) {
+                // Create a div for the subscriber to replace
+                var parentDiv = document.getElementById("subscribers");
+                var subscriberDiv = document.createElement("div");
+                subscriberDiv.id = "opentok_subscriber_" + stream.streamId;
+                parentDiv.appendChild(subscriberDiv);
+                
+                session.subscribe(stream, subscriberDiv.id);
+            }
+        </script>
+
+    </head>
+
+    <body>
+    </body>
 </html>
--- test_server.php
+++ test_server.php
@@ -1,4 +1,7 @@
+<html>
 <?php
+    ini_set('display_errors', 'On');
+    error_reporting(E_ALL);
     require_once 'SDK/OpenTokSDK.php';
     require_once 'SDK/OpenTokArchive.php';
     require_once 'SDK/OpenTokSession.php';
@@ -12,8 +15,6 @@
     // Enable p2p connections
     $session = $apiObj->createSession( $_SERVER["REMOTE_ADDR"], array(SessionPropertyConstants::P2P_PREFERENCE=> "enabled") );
     $sessionID = $session->getSessionID();
-    print $sessionID;
-    print '\n';
-    $token = $apiObj.generate_token($sessionID)
-    print $token
+    $token = $apiObj->generateToken($sessionID);
 ?>
+</html>
