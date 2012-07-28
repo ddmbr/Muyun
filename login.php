57451fc9ea4dcf0709d5ba25dd0f2d6400607bcc
ddmbr <iddmbr@gmail.com>
2012-07-29 00:10:22 +0800

    begin to work on login

---
+++ login.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="assets/css/bootstrap.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container">
+            <label>User Name</label>
+            <input type="text" class="span3" placeholder="Type something…">
+        </div>
+    </body>
+</html>
