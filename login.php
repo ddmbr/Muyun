f4d9fa770617606595e6fb18fefed4ed78a8e289
ddmbr <iddmbr@gmail.com>
2012-07-29 12:04:36 +0800

    modify login

--- login.php
+++ login.php
@@ -1,12 +1,40 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <link href="assets/css/bootstrap.css" rel="stylesheet">
+        <link href="assets/css/bootstrap.css" rel="stylesheet" />
+        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
     </head>
     <body>
         <div class="container">
-            <label>User Name</label>
-            <input type="text" class="span3" placeholder="Type something…">
+            <form class="well" id="loginform">
+                <label>Username or Email</label>
+                <input type="text" class="input-large" id="username" name="username"/>
+                <label>Password</label>
+                <input type="password" class="input-large" id="password" name="password"/>
+                <br />
+                <button type="submit" class="btn btn-primary">Sign in</button>
+            </form>
         </div>
+        <script>
+            $("#loginform").bind("submit", function () {
+                $.ajax({
+                   url: "http://omegaga.net:8000/login/",
+                   type: "post",
+                   cache: false,
+                   dataType: "json", // for example
+                   //data: $(this).serialize(),
+                   success: function (data) { // data is the JS object based on the JSON returned
+                      if (data.success) {
+                         //$(this).fadeOut();
+                        alert(data)
+                      } else {
+                         alert("bad login: "+data.error);
+                      }
+                   }
+                });
+
+                return false;
+            });
+        </script>
     </body>
 </html>
